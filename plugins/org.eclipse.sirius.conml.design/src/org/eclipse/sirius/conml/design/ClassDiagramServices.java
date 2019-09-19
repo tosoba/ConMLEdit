153cd8d500f914dc7c16e3a6420207003044ea7d
TRM <therealmerengue@gmail.com>
2019-09-19 21:41:41 +0200

    Associations work

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/ClassDiagramServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/ClassDiagramServices.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.ecore.EObject;
+
+import conml.types.Association;
 import conml.types.Attribute;
 import conml.types.EnumeratedType;
 import conml.types.Feature;
 import conml.types.Property;
+import conml.types.SemiAssociation;
+import conml.types.TypeModel;
+import conml.types.TypesFactory;
+import conml.types.Class;
 
 public class ClassDiagramServices extends AbstractDiagramServices {
 	public String attributeLabel(final Feature feature) {
@@ -65,5 +72,51 @@ public class ClassDiagramServices extends AbstractDiagramServices {
 		}
 
 		return sb.toString();
+	}
+
+	public String associationBeginLabel(final Association association) {
+		return association.getHasPrimary().getRole();
+	}
+
+	public String associationEndLabel(final Association association) {
+		return association.getHasSecondary().getRole();
+	}
+
+	public Association createAssociation(EObject object, Class source, Class target, EObject sourceView,
+			EObject targetView) {
+		Association association = TypesFactory.eINSTANCE.createAssociation();
+		association.setDefinition("");
+		String primaryName = source.getName() + "s";
+		association.setName(primaryName);
+		SemiAssociation primary = TypesFactory.eINSTANCE.createSemiAssociation();
+		primary.setName(primaryName);
+		primary.setRole(primaryName);
+		primary.setIsPrimaryIn(association);
+		association.setHasPrimary(primary);
+
+		SemiAssociation secondary = TypesFactory.eINSTANCE.createSemiAssociation();
+		String secondaryName = target.getName() + "s";
+		secondary.setName(secondaryName);
+		secondary.setRole(secondaryName);
+		secondary.setIsSecondaryIn(association);
+		association.setHasSecondary(secondary);
+
+		source.getOwnsSemiassociations().add(primary);
+		
+		target.getOwnsSemiassociations().add(secondary);
+
+		if (source.eContainer() instanceof TypeModel) {
+			TypeModel typeModel = (TypeModel) source.eContainer();
+			typeModel.getOwnsElements().add(association);
+		}
+		return association;
+	}
+	
+	public Class getSourceType(Association association) {
+		return association.getHasPrimary() != null ? association.getHasPrimary().getOwner() : null;
+	}
+	
+	public Class getTargetType(Association association) {
+		return association.getHasSecondary() != null ? association.getHasSecondary().getOwner() : null;
 	}
 }
