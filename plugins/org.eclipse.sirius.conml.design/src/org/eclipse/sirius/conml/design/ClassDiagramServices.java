de76070c040cda6b03b3715203e0a4fac18d7b72
TRM <therealmerengue@gmail.com>
2019-09-21 11:30:27 +0200

    Generalizations setup

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/ClassDiagramServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/ClassDiagramServices.java
@@ -1,6 +1,7 @@
 package org.eclipse.sirius.conml.design;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -8,13 +9,14 @@ import org.eclipse.emf.ecore.EObject;
 
 import conml.types.Association;
 import conml.types.Attribute;
+import conml.types.Class;
 import conml.types.EnumeratedType;
 import conml.types.Feature;
+import conml.types.Generalization;
 import conml.types.Property;
 import conml.types.SemiAssociation;
 import conml.types.TypeModel;
 import conml.types.TypesFactory;
-import conml.types.Class;
 
 public class ClassDiagramServices extends AbstractDiagramServices {
 	public String attributeLabel(final Feature feature) {
@@ -102,7 +104,7 @@ public class ClassDiagramServices extends AbstractDiagramServices {
 		association.setHasSecondary(secondary);
 
 		source.getOwnsSemiassociations().add(primary);
-		
+
 		target.getOwnsSemiassociations().add(secondary);
 
 		if (source.eContainer() instanceof TypeModel) {
@@ -111,11 +113,46 @@ public class ClassDiagramServices extends AbstractDiagramServices {
 		}
 		return association;
 	}
-	
+
+	public Generalization createGeneralization(EObject object, Class source, Class target, EObject sourceView,
+			EObject targetView) {
+		// TODO: figure out what to do to prevent one class inheriting from more than
+		// one class (if it's not compliant with ConML)
+		// and maybe also what to do when user attempts to create generalization between
+		// the same 2 classes but in the opposite direction (this also probably should
+		// not be possible)
+		Generalization generalization = TypesFactory.eINSTANCE.createGeneralization();
+		generalization.setDiscriminant("discriminant"); // TODO: change this
+		generalization.getSpecializedClass().add(source);
+		source.getGeneralization().add(generalization);
+		if (generalization.getGeneralizedClass() == null) {
+			generalization.setGeneralizedClass(target);
+		}
+		if (target.getSpecialization() == null) {
+			target.setSpecialization(generalization);
+		}
+		if (source.eContainer() instanceof TypeModel) {
+			TypeModel typeModel = (TypeModel) source.eContainer();
+			typeModel.getOwnsElements().add(generalization);
+		}
+		return generalization;
+	}
+
+	public Collection<EObject> getGeneralizationSourceType(Generalization generalization) {
+		return generalization.getSpecializedClass().stream().map(clazz -> (EObject) clazz).collect(Collectors.toList());
+		// return generalization.getSpecializedClass().isEmpty() ? null :
+		// generalization.getSpecializedClass().get(generalization.getSpecializedClass().size()
+		// - 1);
+	}
+
+	public Class getGeneralizationTargetType(Generalization generalization) {
+		return generalization.getGeneralizedClass();
+	}
+
 	public Class getSourceType(Association association) {
 		return association.getHasPrimary() != null ? association.getHasPrimary().getOwner() : null;
 	}
-	
+
 	public Class getTargetType(Association association) {
 		return association.getHasSecondary() != null ? association.getHasSecondary().getOwner() : null;
 	}
