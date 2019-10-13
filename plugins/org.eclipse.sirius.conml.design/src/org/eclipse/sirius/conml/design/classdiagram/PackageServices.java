2f4a086b3e41b1298b454314aa617c515780f21c
Mere Ngue <therealmerengue@gmail.com>
2019-10-13 12:17:06 +0200

    Adding/removing packages from list.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/classdiagram/PackageServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/classdiagram/PackageServices.java
@@ -4,12 +4,43 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.sirius.conml.design.ConML;
 
 import conml.Model;
+import conml.types.Class;
+import conml.types.EnumeratedType;
 import conml.types.Package;
+import conml.types.TypeModel;
 
 public class PackageServices {
+
+  public void removePackage(EObject object) {
+    if (!(object instanceof Package)) return;
+    final Package _package = (Package) object;
+    final EObject container = _package.eContainer();
+    if (!(container instanceof TypeModel)) return;
+    final TypeModel typeModel = (TypeModel) container;
+
+    typeModel
+        .getElements()
+        .forEach(
+            element -> {
+              if (element instanceof Class) {
+                Class clazz = (Class) element;
+                if (EcoreUtil.equals(_package, clazz.getPackage())) {
+                  clazz.setPackage(null);
+                }
+              } else if (element instanceof EnumeratedType) {
+                EnumeratedType enumType = (EnumeratedType) element;
+                if (EcoreUtil.equals(enumType.getPackage(), _package)) {
+                  enumType.setPackage(null);
+                }
+              }
+            });
+
+    typeModel.getElements().remove(_package);
+  }
 
   public String getPackageName(EObject object) {
     if (!(object instanceof Package)) return "";
