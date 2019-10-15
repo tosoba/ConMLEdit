0c66e35dddd3e776d2f498fb7512b340e3962813
Mere Ngue <therealmerengue@gmail.com>
2019-10-15 15:59:06 +0200

    Package names in class label.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/classdiagram/ClassServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/classdiagram/ClassServices.java
@@ -2,14 +2,17 @@ package org.eclipse.sirius.conml.design.classdiagram;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.sirius.conml.design.ConML;
 
 import conml.Model;
 import conml.types.Class;
+import conml.types.Package;
 import conml.types.TypeModel;
 
 public class ClassServices extends ModelElementServices {
@@ -42,27 +45,37 @@ public class ClassServices extends ModelElementServices {
   }
 
   public String classLabel(Class clazz) {
-    if (clazz == null) {
-      return "";
-    }
+    if (clazz == null) return "";
 
     final StringBuilder sb = new StringBuilder();
     final ArrayList<String> markers = new ArrayList<>();
-    if (clazz.isUsedAsSubjectiveAspect()) {
-      markers.add("S");
-    }
-    if (clazz.isUsedAsTemporalAspect()) {
-      markers.add("T");
-    }
-    if (!markers.isEmpty()) {
+    if (clazz.isUsedAsSubjectiveAspect()) markers.add("S");
+    if (clazz.isUsedAsTemporalAspect()) markers.add("T");
+    if (!markers.isEmpty())
       sb.append('[').append(markers.stream().collect(Collectors.joining(","))).append("] ");
+
+    Package packageIterator = clazz.getPackage();
+    final ArrayList<String> packageNames = new ArrayList<>();
+    boolean containedInOverallPackage = false;
+    while (packageIterator != null) {
+      if (EcoreUtil.equals(packageIterator, packageIterator.getContainerPackage())) {
+        break; // to prevent endless loop in case user makes a mistake (self-containment)
+      } else if (packageIterator.isOverall()) {
+        containedInOverallPackage = true;
+        break;
+      }
+      packageNames.add(packageIterator.getName());
+      packageIterator = packageIterator.getContainerPackage();
+    }
+    if (!packageNames.isEmpty()) {
+      if (containedInOverallPackage) sb.append('.');
+      Collections.reverse(packageNames);
+      sb.append(packageNames.stream().collect(Collectors.joining("."))).append('.');
     }
 
     sb.append(clazz.getName());
 
-    if (clazz.isAbstract()) {
-      sb.append(" (A)");
-    }
+    if (clazz.isAbstract()) sb.append(" (A)");
 
     return sb.toString();
   }
