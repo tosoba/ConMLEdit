bfb99d9b63d9025011d8314052229f94f3cb7607
Mere Ngue <therealmerengue@gmail.com>
2019-10-25 20:05:02 +0200

    Objects list setup.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/objectdiagram/InstanceModelServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/objectdiagram/InstanceModelServices.java
@@ -12,4 +12,8 @@ public class InstanceModelServices extends ModelServices {
   public Collection<EObject> getOwnedLinkElements(EObject object) {
     return getOwnedElementsOfType(object, Link.class);
   }
+
+  public Collection<EObject> getOwnedObjectElements(EObject object) {
+    return getOwnedElementsOfType(object, conml.instances.Object.class);
+  }
 }
--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/objectdiagram/ObjectServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/objectdiagram/ObjectServices.java
@@ -1,12 +1,24 @@
 package org.eclipse.sirius.conml.design.objectdiagram;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.conml.design.ConML;
+import org.eclipse.sirius.conml.design.classdiagram.ModelElementServices;
+
 import conml.instances.Object;
 
-public class ObjectServices {
+public class ObjectServices extends ModelElementServices {
 
   public String objectLabel(Object object) {
     return object.getIdentifier()
         + ": "
         + (object.getInstancedClass() != null ? object.getInstancedClass().getName() : "?");
+  }
+
+  public void moveObjectUp(final EObject object) {
+    moveElement(object, Object.class, ConML.ElementMovementDirection.UP);
+  }
+
+  public void moveObjectDown(final EObject object) {
+    moveElement(object, Object.class, ConML.ElementMovementDirection.DOWN);
   }
 }
