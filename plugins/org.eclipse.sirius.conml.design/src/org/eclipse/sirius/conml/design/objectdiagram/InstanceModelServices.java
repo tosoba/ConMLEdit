0b5398d5d5cd4fd1dac49be6d31606eb9887a87a
Mere Ngue <therealmerengue@gmail.com>
2019-10-25 22:14:32 +0200

    InstanceModel conforming to TypeModel validation.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/objectdiagram/InstanceModelServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/objectdiagram/InstanceModelServices.java
@@ -3,8 +3,10 @@ package org.eclipse.sirius.conml.design.objectdiagram;
 import java.util.Collection;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.conml.design.ConML;
 import org.eclipse.sirius.conml.design.ModelServices;
 
+import conml.instances.InstanceModel;
 import conml.instances.Link;
 
 public class InstanceModelServices extends ModelServices {
@@ -15,5 +17,13 @@ public class InstanceModelServices extends ModelServices {
 
   public Collection<EObject> getOwnedObjectElements(final EObject object) {
     return getOwnedElementsOfType(object, conml.instances.Object.class);
+  }
+
+  public boolean instanceModelConformsToAtLeastOneTypeModel(final EObject object) {
+    return ConML.castAndRunOrReturn(
+        object,
+        InstanceModel.class,
+        instanceModel -> !instanceModel.getConformedTypeModels().isEmpty(),
+        true);
   }
 }
