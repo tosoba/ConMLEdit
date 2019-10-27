0f6174ddb80bf2451a0dfa404f87c27c0cd1d78e
Mere Ngue <therealmerengue@gmail.com>
2019-10-27 18:14:34 +0100

    Gen project setup.

---
+++ plugins/org.eclipse.sirius.conml.gen/src/org/eclipse/sirius/conml/gen/Activator.java
@@ -0,0 +1,57 @@
+package org.eclipse.sirius.conml.gen;
+
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Plugin {
+
+  /** The plug-in ID. */
+  public static final String PLUGIN_ID = "fr.obeo.dsl.arduino.gen";
+
+  /** The shared instance. */
+  private static Activator plugin;
+
+  /** The constructor. */
+  public Activator() {}
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    plugin = this;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception {
+    plugin = null;
+    super.stop(context);
+  }
+
+  /**
+   * Returns the shared instance.
+   *
+   * @return the shared instance
+   */
+  public static Activator getDefault() {
+    return plugin;
+  }
+
+  /**
+   * A helper to log plugin errors.
+   *
+   * @param severity the error severity.
+   * @param message the error message.
+   * @param exception the error exception.
+   */
+  public static void log(int severity, String message, Throwable exception) {
+    getDefault().getLog().log(new Status(severity, PLUGIN_ID, message, exception));
+  }
+}
