be111d78365b31561b73a3398434eda497dc33d5
therealmerengue <therealmerengue@gmail.com>
2019-11-17 14:25:34 +0100

    More changes.to mirroring objects

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/services/objectdiagram/ObjectServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/services/objectdiagram/ObjectServices.java
@@ -1,11 +1,14 @@
 package org.eclipse.sirius.conml.design.services.objectdiagram;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.eclipse.emf.ecore.util.EcoreUtil;
@@ -15,12 +18,18 @@ import org.eclipse.sirius.conml.design.util.ConML;
 import org.eclipse.sirius.conml.design.util.messages.Messages;
 import org.eclipse.sirius.diagram.DSemanticDiagram;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
+
 import conml.MetaInformation;
 import conml.Model;
 import conml.instances.InstanceModel;
+import conml.instances.InstancesFactory;
 import conml.instances.Link;
 import conml.instances.Object;
 import conml.instances.Reference;
+import conml.instances.ReferenceSet;
+import conml.instances.ValueSet;
 import conml.types.Attribute;
 import conml.types.Class;
 
@@ -110,14 +119,97 @@ public class ObjectServices {
         .anyMatch(model -> EcoreUtil.equals(model, containerObject));
   }
 
+  private EContentAdapter objectChangesAdapter(final Object subscriberObject) {
+    return new EContentAdapter() {
+      @Override
+      public void notifyChanged(Notification notification) {
+        super.notifyChanged(notification);
+        if (!(notification.getNotifier() instanceof EObject)) return;
+        final EObject notifier = (EObject) notification.getNotifier();
+        if (!(notifier.eContainer() instanceof InstanceModel)) return;
+
+        if (notification.getFeature() instanceof EStructuralFeature) {
+          notifier.eSetDeliver(false);
+          subscriberObject.eSetDeliver(false);
+          final EStructuralFeature feature = (EStructuralFeature) notification.getFeature();
+          switch (notification.getEventType()) {
+            case Notification.SET:
+              subscriberObject.eSet(feature, notification.getNewValue());
+              break;
+            case Notification.UNSET:
+              subscriberObject.eUnset(feature);
+              break;
+
+            case Notification.ADD:
+            case Notification.ADD_MANY:
+              if (feature instanceof EReference) {
+                if ("ObjectMetaInformation".equalsIgnoreCase(feature.getName())) return;
+
+                final EReference reference = (EReference) feature;
+                Set<EObject> notificationValues = getNotificationValues(notification);
+                if ("ValueSets".equalsIgnoreCase(reference.getName())) {
+                  subscriberObject
+                      .getValueSets()
+                      .addAll(
+                          notificationValues
+                              .stream()
+                              .filter(ValueSet.class::isInstance)
+                              .map(ValueSet.class::cast)
+                              .collect(Collectors.toList()));
+                } else if ("ReferenceSets".equalsIgnoreCase(reference.getName())) {
+                  subscriberObject
+                      .getReferenceSets()
+                      .addAll(
+                          notificationValues
+                              .stream()
+                              .filter(ReferenceSet.class::isInstance)
+                              .map(ReferenceSet.class::cast)
+                              .collect(Collectors.toList()));
+                }
+              }
+              break;
+
+            case Notification.REMOVE:
+            case Notification.REMOVE_MANY:
+              if (feature instanceof EReference) {
+                if ("ObjectMetaInformation".equalsIgnoreCase(feature.getName())) return;
+
+                final EReference reference = (EReference) feature;
+                Set<EObject> notificationValues = getNotificationValues(notification);
+                if ("ValueSets".equalsIgnoreCase(reference.getName())) {
+                  subscriberObject.getValueSets().removeAll(notificationValues);
+                } else if ("ReferenceSets".equalsIgnoreCase(reference.getName())) {
+                  subscriberObject.getReferenceSets().removeAll(notificationValues);
+                }
+              }
+              break;
+            case Notification.MOVE:
+              break;
+          }
+
+          notifier.eSetDeliver(true);
+          subscriberObject.eSetDeliver(true);
+        }
+      };
+    };
+  }
+
   public void addMetaInfoObjectToModels(
       final Object object,
       final InstanceModel instanceModel,
       final Model model,
       final MetaInformation metaInfo) {
-    final Object instanceModelObject = EcoreUtil.copy(object);
-    instanceModelObject.setMirroredMetaInfoObject(object);
+    final Object instanceModelObject = InstancesFactory.eINSTANCE.createObject();
+    instanceModelObject.setIdentifier(object.getIdentifier());
+    instanceModelObject.setCertainty(object.getCertainty());
     instanceModelObject.setInstancedClass(object.getInstancedClass());
+    instanceModelObject.setTemporalExistentialQualifier(object.getTemporalExistentialQualifier());
+    instanceModelObject.setSubjectiveExistentialQualifer(object.getSubjectiveExistentialQualifer());
+    instanceModelObject.setMirroredMetaInfoObject(object);
+
+    instanceModelObject.getValueSets();
+    instanceModelObject.getReferenceSets();
+
     object.getInstancedClass().getInstanceObjects().add(instanceModelObject);
     instanceModel.getElements().add(instanceModelObject);
 
@@ -126,47 +218,25 @@ public class ObjectServices {
     metaInfo.setModel(model);
     object.getObjectMetaInformation().add(metaInfo);
 
-    EContentAdapter objectContentAdapter =
-        new EContentAdapter() {
-          @Override
-          public void notifyChanged(Notification notification) {
-            super.notifyChanged(notification);
-            if (!(notification.getNotifier() instanceof EObject)) return;
-            final EObject notifier = (EObject) notification.getNotifier();
-            if (notifier.eContainer() == null) return;
+    object.eAdapters().add(objectChangesAdapter(instanceModelObject));
+    instanceModelObject.eAdapters().add(objectChangesAdapter(object));
+  }
 
-            if (notification.getFeature() instanceof EStructuralFeature) {
-              instanceModelObject.eSetDeliver(false);
-              final EStructuralFeature feature = (EStructuralFeature) notification.getFeature();
-              if (feature.getUpperBound() == 1)
-                instanceModelObject.eSet(
-                    (EStructuralFeature) notification.getFeature(), notification.getNewValue());
-              instanceModelObject.eSetDeliver(true);
-            }
-          };
-        };
-    object.eAdapters().add(objectContentAdapter);
-
-    EContentAdapter instanceModelObjectContentAdapter =
-        new EContentAdapter() {
-          @Override
-          public void notifyChanged(Notification notification) {
-            super.notifyChanged(notification);
-            if (!(notification.getNotifier() instanceof EObject)) return;
-            final EObject notifier = (EObject) notification.getNotifier();
-            if (notifier.eContainer() == null) return;
-
-            if (notification.getFeature() instanceof EStructuralFeature) {
-              object.eSetDeliver(false);
-              final EStructuralFeature feature = (EStructuralFeature) notification.getFeature();
-              if (feature.getUpperBound() == 1)
-                object.eSet(
-                    (EStructuralFeature) notification.getFeature(), notification.getNewValue());
-              object.eSetDeliver(true);
-            }
-          };
-        };
-    instanceModelObject.eAdapters().add(instanceModelObjectContentAdapter);
+  private Set<EObject> getNotificationValues(Notification notification) {
+    final Set<EObject> values = Sets.newLinkedHashSet();
+    java.lang.Object value = notification.getOldValue();
+    if ((notification.getEventType() == Notification.ADD
+            || notification.getEventType() == Notification.ADD_MANY)
+        && notification.getFeature() instanceof EReference
+        && notification.getNewValue() != null) {
+      value = notification.getNewValue();
+    }
+    if (value instanceof Collection) {
+      Iterables.addAll(values, Iterables.filter((Collection<?>) value, EObject.class));
+    } else if (value instanceof EObject) {
+      values.add((EObject) value);
+    }
+    return values;
   }
 
   public boolean shouldShowObjectInObjectDiagram(
