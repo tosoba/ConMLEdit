cf62cc4e9c4c5b4d1e0022ea9a263ec47df33b97
Mere Ngue <therealmerengue@gmail.com>
2019-11-20 16:46:42 +0100

    More dialog changes.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/dialog/EObjectSelectionDialog.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/dialog/EObjectSelectionDialog.java
@@ -8,16 +8,11 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.sirius.conml.design.Activator;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.FilteredItemsSelectionDialog;
 
@@ -30,40 +25,25 @@ public abstract class EObjectSelectionDialog<T extends EObject>
 
   private static final String SELECTION_DIALOG_SETTINGS = "SelectionDialogSettings";
 
-  private final String headerText;
-
   private ILabelProvider detailsLabelProvider;
   private ILabelProvider listLabelProvider;
 
   public EObjectSelectionDialog(
       Shell shell,
       final Collection<T> partsToSelect,
-      final String headerText,
       final ILabelProvider listLabelProvider,
       final ILabelProvider detailsLabelProvider) {
     super(shell);
     this.partsToSelect = partsToSelect;
-    this.headerText = headerText;
     this.listLabelProvider = listLabelProvider;
     this.detailsLabelProvider = detailsLabelProvider;
   }
 
+  protected void createHeader(Composite parent) {}
+
   @Override
   protected Control createDialogArea(Composite parent) {
-    final GridLayout layout = new GridLayout(1, false);
-    layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
-    layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
-    layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
-    layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
-
-    final Composite compositeHeader = new Composite(parent, SWT.NONE);
-    compositeHeader.setLayout(layout);
-    final Label headerLabel = new Label(compositeHeader, SWT.NONE);
-    headerLabel.setText(headerText);
-    final GridData gridData = new GridData();
-    gridData.horizontalAlignment = GridData.FILL;
-    gridData.horizontalSpan = 2;
-    headerLabel.setLayoutData(gridData);
+    createHeader(parent);
 
     final Control dialogArea = super.createDialogArea(parent);
     dialogArea.setEnabled(true);
--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/dialog/TypeModelSelectionDialog.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/dialog/TypeModelSelectionDialog.java
@@ -3,10 +3,15 @@ package org.eclipse.sirius.conml.design.dialog;
 import java.util.Collection;
 import java.util.Comparator;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.sirius.conml.design.util.Comparators;
 import org.eclipse.sirius.conml.design.util.LabelProviders;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 import conml.NamedElement;
@@ -14,21 +19,48 @@ import conml.types.TypeModel;
 
 public final class TypeModelSelectionDialog extends EObjectSelectionDialog<TypeModel> {
 
+  private final String headerText;
+
+  @Override
+  protected void createButtonsForButtonBar(Composite parent) {
+    super.createButtonsForButtonBar(parent);
+    // TODO: maybe move new TypeModel button here
+  }
+
   public TypeModelSelectionDialog(
       Shell shell, final Collection<TypeModel> partsToSelect, final String headerText) {
     super(
         shell,
         partsToSelect,
-        headerText,
         LabelProviders.namedElementLabelProvider(),
         LabelProviders.describableElementLabelProvider());
+    this.headerText = headerText;
   }
 
   @Override
-  protected Control createExtendedContentArea(Composite parent) {
-    // TODO: new TypeModel button opening simple TypeModel creation dialog with name, version,
-    // description and defaultLanguage
-    return super.createExtendedContentArea(parent);
+  protected void createHeader(Composite parent) {
+    final Composite compositeHeader = new Composite(parent, SWT.NONE);
+    final GridLayout layout = new GridLayout(2, false);
+    layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
+    layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+    layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
+    layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+    compositeHeader.setLayout(layout);
+
+    final Label headerLabel = new Label(compositeHeader, SWT.NONE);
+    headerLabel.setText(headerText);
+    GridData gridData = new GridData();
+    gridData.horizontalAlignment = GridData.FILL;
+    gridData.horizontalSpan = 1;
+    headerLabel.setLayoutData(gridData);
+
+    //TODO: move create TypeModel button to the right
+    final Button button = new Button(compositeHeader, SWT.NONE);
+    button.setText("New TypeModel");
+    gridData = new GridData();
+    gridData.horizontalAlignment = GridData.HORIZONTAL_ALIGN_END;
+    gridData.horizontalSpan = 1;
+    button.setLayoutData(gridData);
   }
 
   @Override
