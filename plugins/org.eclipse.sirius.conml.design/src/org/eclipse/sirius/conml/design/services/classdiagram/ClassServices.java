8fa153001b5c6495c51b8344168b4b753adfeab9
therealmerengue <therealmerengue@gmail.com>
2019-11-24 18:23:40 +0100

    Adding classes in Object Diagram.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/services/classdiagram/ClassServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/services/classdiagram/ClassServices.java
@@ -1,7 +1,10 @@
 package org.eclipse.sirius.conml.design.services.classdiagram;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EObject;
@@ -9,6 +12,7 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.sirius.conml.design.dialog.ClassSelectionDialog;
 import org.eclipse.sirius.conml.design.dialog.ExistingSemanticElementsSelectionDialog;
 import org.eclipse.sirius.conml.design.services.ExistingElementsServices;
+import org.eclipse.sirius.conml.design.services.UIServices;
 import org.eclipse.sirius.conml.design.services.objectdiagram.ObjectServices;
 import org.eclipse.sirius.conml.design.util.ConML;
 import org.eclipse.sirius.conml.design.util.ConMLPredicates;
@@ -18,6 +22,7 @@ import org.eclipse.sirius.diagram.DDiagram;
 import conml.Domain;
 import conml.types.Association;
 import conml.types.Class;
+import conml.types.Generalization;
 import conml.types.SemiAssociation;
 
 public final class ClassServices {
@@ -108,5 +113,57 @@ public final class ClassServices {
                 false);
     if (result.size() == 1 && result.get(0) instanceof Class) return (Class) result.get(0);
     else return null;
+  }
+
+  public void showClassSelectionDialogForObjectDiagram(
+      final EObject selectedContainer,
+      final EObject selectedContainerView,
+      final DDiagram diagram) {
+    final Set<Class> displayedObjectsInstancedClasses = new HashSet<>();
+    final Set<Class> displayedClasses = new HashSet<>();
+    UIServices.getInstance()
+        .getDisplayedNodes(diagram)
+        .forEach(
+            obj -> {
+              if (obj instanceof conml.instances.Object) {
+                final conml.instances.Object object = (conml.instances.Object) obj;
+                if (object.getInstancedClass() != null)
+                  displayedObjectsInstancedClasses.add(object.getInstancedClass());
+              } else if (obj instanceof Class) {
+                displayedClasses.add((Class) obj);
+              }
+            });
+    final Set<Class> displayedClassesGeneralizedClasses =
+        displayedClasses
+            .stream()
+            .map(Class::getGeneralizations)
+            .flatMap(Collection::stream)
+            .map(Generalization::getGeneralizedClass)
+            .collect(Collectors.toSet());
+
+    ExistingElementsServices.getInstance()
+        .openSelectExistingElementsDialogAndAddElements(
+            selectedContainer,
+            selectedContainerView,
+            diagram,
+            new ClassSelectionDialog(
+                Messages.getString("Dialog.SelectClass"),
+                Messages.getString("Dialog.SelectClassToAddToObjectDiagram"),
+                (Object obj) -> {
+                  if (!(obj instanceof Class)) return false;
+
+                  final Class classCandidate = (Class) obj;
+                  if (classCandidate.getTypeModel() == null
+                      || displayedClasses.contains(classCandidate)) {
+                    return false;
+                  }
+                  if (displayedObjectsInstancedClasses.contains(classCandidate)
+                      || displayedClassesGeneralizedClasses.contains(classCandidate)) {
+                    return true;
+                  }
+
+                  return false;
+                }),
+            Class.class);
   }
 }
