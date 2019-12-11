1e84ddc4cf0c512c6e238845364d5c09ae21d441
Mere Ngue <therealmerengue@gmail.com>
2019-12-11 13:18:39 +0100

    Creating new diagram types from dashboard.

---
+++ plugins/org.eclipse.sirius.conml.dashboard/src/org/eclipse/sirius/conml/dashboard/diagram/CreateModelDiagram.java
@@ -0,0 +1,11 @@
+package org.eclipse.sirius.conml.dashboard.diagram;
+
+import org.eclipse.amalgam.explorer.activity.ui.api.hyperlinkadapter.NewDiagramHyperLinkAdapter;
+
+public final class CreateModelDiagram extends NewDiagramHyperLinkAdapter {
+
+  @Override
+  public String getRepresentationName() {
+    return "Model Diagram";
+  }
+}
