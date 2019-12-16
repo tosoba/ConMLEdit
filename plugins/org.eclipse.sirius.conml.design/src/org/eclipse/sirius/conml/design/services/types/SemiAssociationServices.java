59e250bf20a7e5c1040622b521e5314793487f8d
Tomasz Osoba <tomasz.osoba@comarch.pl>
2019-12-16 16:25:42 +0100

    Redefined semiAssociation creation.

--- plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/services/types/SemiAssociationServices.java
+++ plugins/org.eclipse.sirius.conml.design/src/org/eclipse/sirius/conml/design/services/types/SemiAssociationServices.java
@@ -1,13 +1,19 @@
 package org.eclipse.sirius.conml.design.services.types;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.sirius.conml.design.Activator;
 import org.eclipse.sirius.conml.design.util.ConML;
 import org.eclipse.sirius.conml.design.util.messages.Messages;
 
+import conml.Domain;
 import conml.types.Association;
+import conml.types.Class;
 import conml.types.SemiAssociation;
+import conml.types.TypesFactory;
 
 public final class SemiAssociationServices {
 
@@ -17,6 +23,14 @@ public final class SemiAssociationServices {
 
   public static SemiAssociationServices getInstance() {
     return InstanceHolder.INSTANCE;
+  }
+
+  public Class getReferredClass(final SemiAssociation semi) {
+    return semi.getReferredClass();
+  }
+
+  public void setReferredClass(final SemiAssociation semi, final Class clazz) {
+    semi.setReferredClass(clazz);
   }
 
   public void deleteCompactSemiAssociation(final SemiAssociation semi) {
@@ -77,5 +91,98 @@ public final class SemiAssociationServices {
 
   public void setStrong(final SemiAssociation semiAssociation, final Boolean strong) {
     semiAssociation.setStrong(strong);
+  }
+
+  public void setRole(final SemiAssociation semiAssociation, final String role) {
+    semiAssociation.setRole(role);
+  }
+
+  public Set<Class> getRedefinedSemiAssociationReferredClassCandidates(
+      final SemiAssociation semiAssociation) {
+    final SemiAssociation redefined = semiAssociation.getRedefinedSemiAssociation();
+    if (redefined == null || redefined.getReferredClass() == null) return new HashSet<>();
+    return getDescendantsOfClass(redefined.getReferredClass());
+  }
+
+  private Set<Class> getDescendantsOfClass(final Class clazz) {
+    final Set<Class> currentClasses = new HashSet<>();
+    getDescendantsOfClass(clazz, currentClasses);
+    return currentClasses;
+  }
+
+  private void getDescendantsOfClass(final Class clazz, final Set<Class> currentClasses) {
+    currentClasses.add(clazz);
+    if (clazz.getSpecialization() == null) return;
+    clazz
+        .getSpecialization()
+        .getSpecializedClasses()
+        .forEach(specializedClass -> getDescendantsOfClass(specializedClass, currentClasses));
+  }
+
+  public void createInverseAndAddRedefinedSemiToAssociation(final SemiAssociation semi) {
+    final SemiAssociation redefined = semi.getRedefinedSemiAssociation();
+    if (redefined == null || redefined.getInverseSemiAssociation() == null) {
+      Activator.logError(
+          new IllegalStateException(
+              Messages.getString(
+                  "ExceptionMessage.IsNull", "Redefined SemiAssociation or its inverse")));
+      return;
+    }
+
+    boolean createPrimary;
+    Association redefinedAssociation;
+    if (redefined.getPrimaryInAssociation() != null) {
+      createPrimary = false;
+      redefinedAssociation = redefined.getPrimaryInAssociation();
+    } else if (redefined.getSecondaryInAssociation() != null) {
+      createPrimary = true;
+      redefinedAssociation = redefined.getSecondaryInAssociation();
+    } else {
+      Activator.logError(
+          new IllegalStateException(
+              Messages.getString(
+                  "ExceptionMessage.IsNull",
+                  "Redefined SemiAssociation's primary and secondary Association reference")));
+      return;
+    }
+
+    final Association association = TypesFactory.eINSTANCE.createAssociation();
+    association.setCompact(redefinedAssociation.isCompact());
+    association.setName(redefinedAssociation.getName());
+    association.setDefinition(redefinedAssociation.getDefinition());
+    if (createPrimary) semi.setSecondaryInAssociation(association);
+    else semi.setPrimaryInAssociation(association);
+
+    final SemiAssociation inverseOfRedefined = redefined.getInverseSemiAssociation();
+    if (inverseOfRedefined == null) {
+      Activator.logError(
+          new IllegalStateException(
+              Messages.getString(
+                  "ExceptionMessage.IsNull", "Redefined SemiAssociation's inverse")));
+      return;
+    }
+
+    final SemiAssociation inverseSemi = TypesFactory.eINSTANCE.createSemiAssociation();
+    inverseSemi.setName(inverseOfRedefined.getName());
+    inverseSemi.setDefinition(inverseOfRedefined.getDefinition());
+    inverseSemi.setRole(inverseOfRedefined.getRole());
+    inverseSemi.setReferredClass(inverseOfRedefined.getReferredClass());
+    inverseSemi.setConstant(inverseOfRedefined.isConstant());
+    inverseSemi.setMaximumCardinality(inverseOfRedefined.getMaximumCardinality());
+    inverseSemi.setMinimumCardinality(inverseOfRedefined.getMinimumCardinality());
+    inverseSemi.setOwnerClass(inverseOfRedefined.getOwnerClass());
+    inverseSemi.setSorted(inverseOfRedefined.isSorted());
+    inverseSemi.setStrong(inverseOfRedefined.isStrong());
+    inverseSemi.setSubjective(inverseOfRedefined.isSubjective());
+    inverseSemi.setTemporal(inverseOfRedefined.isTemporal());
+    inverseSemi.setWhole(inverseOfRedefined.isWhole());
+
+    semi.setInverseSemiAssociation(inverseSemi);
+
+    if (createPrimary) inverseSemi.setPrimaryInAssociation(association);
+    else inverseSemi.setSecondaryInAssociation(association);
+
+    final Domain domain = (Domain) redefinedAssociation.eContainer();
+    domain.getParts().add(association);
   }
 }
